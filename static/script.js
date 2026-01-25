8235d8fbfa036da24c0acf5df2f328fc02bd3bd2
uw935 <no-reply@uw935.com>
2026-01-25 23:03:33 +0300

    remove: new year decorations

--- static/script.js
+++ static/script.js
@@ -14,19 +14,19 @@ fetch("https://api.npoint.io/662ddff4e44b520c878f", {}).then(
     }
 );
 
-$(document).ready(function() {
-    $("body").flurry({
-        character: ["*"],
-        height: 940,
-        onFlake: function () {
-            $(this).html(
-                '<img width="25" height="25" src="https://uw935.com/static/images/emojicons/snowflake@2x.png" alt="❄️">'
-            );
-        },
-        size: 400,
-        frequency: 50,
-        wind: 100,
-        windVariance: 100,
-        speed: 3000
-    });
-});
+// $(document).ready(function() {
+//     $("body").flurry({
+//         character: ["*"],
+//         height: 940,
+//         onFlake: function () {
+//             $(this).html(
+//                 '<img width="25" height="25" src="https://uw935.com/static/images/emojicons/snowflake@2x.png" alt="❄️">'
+//             );
+//         },
+//         size: 400,
+//         frequency: 50,
+//         wind: 100,
+//         windVariance: 100,
+//         speed: 3000
+//     });
+// });
